07d4b45a060b3741148dd41d322b1c82f0ea46fc
David Underhill <dgu@cs.stanford.edu>
2009-04-20 13:59:54 -0700

    cherry pick adam's ui feedback and remove bg styling

--- html.py
+++ html.py
@@ -5,8 +5,8 @@ def write_header(rh, p=20, m=10, pb=0):
   <head>
     <title>Color Quest</title>
   </head>
-  <body style="background-color:#444444">
-  <div style="background-color:#FFFFFF; border: 10px solid #440000;  margin:%dpx auto %dpx auto; padding:0 %dpx %dpx %dpx; width:1000px">
+  <body style="background-color:#FFFFFF">
+  <div style="margin:%dpx auto %dpx auto; padding:0 %dpx %dpx %dpx; width:1000px">
 """ % (m, m, p, pb, p))
 
 def write_footer(rh):
