alikat/color-quest

commit 37fa3ece95ffb6cdff203ae049c4fd3d937c80e4

disable buttons after a user clicks one -- prevents accidental acceptance of future round(s)' trades!

colorquest.py
@@ -217,7 +217,9 @@ class MainPage(webapp.RequestHandler):
         trade_row_start = '''<tr>
 <td style="width:200px">
   <FORM METHOD="POST" ACTION="/gameplay" style=" padding:0; margin:0">
-    <INPUT type="submit" name="Choice_%u" value="Accept Trade %u" style="height:%upx">%s
+    <INPUT type="submit" id="Choice_%u" name="Choice_%u" value="Accept Trade %u" style="height:%upx"
+           onclick="document.getElementById('Choice_1').disabled=true;
+                    document.getElementById('Choice_2').disabled=true;">%s
   </FORM>
 </td>'''
         warning = '''
@@ -239,7 +241,7 @@ class MainPage(webapp.RequestHandler):
               rh = 50
               extra =  warning % (100.0 - 100.0*game.trade_honesty)
 
-          write(trade_row_start % (t, t, rh, extra))
+          write(trade_row_start % (t, t, t, rh, extra))
           for i in range(NUM_COLORS):
             if HIDE_UNNEEDED_TRADE_CELLS and trade[i]==0:
               write('<td style="background-color:#CCCCCC"></td>')

intro.py
@@ -40,7 +40,7 @@ class MainPage(webapp.RequestHandler):
 
         <center>
 	<FORM METHOD="LINK" ACTION="/gameplay_start.html">
-	<button style="height:50px; width:400px; font:bold 24px Arial;">Play now!</button>
+	<button onclick="this.disabled=true;" style="height:50px; width:400px; font:bold 24px Arial;">Play now!</button>
 	</FORM>""")
 
         self.response.out.write(get_high_scores_html())

commit 0087c62b4a996ca20a990388d7b705a3badee1ed

regression fix: disabling the button prevented its value from be sent   -- regressed in 37fa3ece95ffb6cdff203ae049c4fd3d937c80e4   -- fix is to populate a hidden field with the desired value prior to disabling the button   -- added support to work in non-javascript enabled browsers too

colorquest.py
@@ -45,7 +45,14 @@ class MainPage(webapp.RequestHandler):
         game.round2_choices = game.round2_choices + 1
 
       # get which trade was taken
-      took_trade1 = (self.request.get("Choice_1", '') != '')
+      choiceh = self.request.get("choiceh", '')
+      if choiceh == '':
+        # only works if javascript is off (which is when choiceh fails)
+        took_trade1 = (self.request.get("Choice_1", '') != '')
+      else:
+        took_trade1 = (choiceh == 'Accept Trade 1')
+
+      # get the trade objects
       if took_trade1:
         trade_taken = game.trade1
         trade_not_taken = game.trade2
@@ -215,8 +222,10 @@ class MainPage(webapp.RequestHandler):
         trade_row_start = '''<tr>
 <td style="width:200px">
   <FORM METHOD="POST" ACTION="/gameplay" style=" padding:0; margin:0">
+    <input type="hidden" id="choiceh" name="choiceh" value="nil"/>
     <INPUT type="submit" id="Choice_%u" name="Choice_%u" value="Accept Trade %u" style="height:%upx"
-           onclick="document.getElementById('Choice_1').disabled=true;
+           onclick="document.getElementById('choiceh').value = this.value;
+                    document.getElementById('Choice_1').disabled=true;
                     document.getElementById('Choice_2').disabled=true;">%s
   </FORM>
 </td>'''

intro.py
@@ -40,7 +40,7 @@ class MainPage(webapp.RequestHandler):
 
         <center>
 	<FORM METHOD="LINK" ACTION="/gameplay_start.html">
-	<button onclick="this.disabled=true;" style="height:50px; width:400px; font:bold 24px Arial;">Play now!</button>
+	<button style="height:50px; width:400px; font:bold 24px Arial;">Play now!</button>
 	</FORM>""")
 
         self.response.out.write(get_high_scores_html())