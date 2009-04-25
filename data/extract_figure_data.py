17c48510a7bcbc9f9667d979b68c50d5a84e7457
David Underhill <dgu@cs.stanford.edu>
2009-04-25 03:46:44 -0700

    generate ALL row as well as multiple tables for different round 2 thresholds

--- data/extract_figure_data.py
+++ data/extract_figure_data.py
@@ -17,25 +17,36 @@ MIN_R2_CHOICES = 5
 # which confidence intervals to generate (only valid choices are 90, 95, 99, and 9995)
 INTERVALS = [95, 9995]
 
-def simple_stats_table(out, precision):
+def simple_stats_table(out, precision, c):
     """
     Computes some basic statistics about the estimated impact of the
     zero-phenomenon for various round 1 rationality thresholds.
+    @param c  minimum number of choices required for consideration
     """
     print >> out, ResAcc.get_latex_table_header('|c|c', '\\textbf{\\# Rational in Round 1} & \\textbf{\\# Samples} & ')
-    for num_rational_in_r1 in range(3,MAX_R1_CHOICES+1):
-        data = get_rational_data_discrete(data_first_and_fin, num_rational_in_r1, num_rational_in_r1, MIN_R2_CHOICES)
+    for num_rational_in_r1 in range(0,MAX_R1_CHOICES+2):
+        if num_rational_in_r1 == MAX_R1_CHOICES+1:
+            l = 3
+            u = 13
+            title = 'ALL'
+            print >> out, '\\hline'
+        else:
+            l = num_rational_in_r1
+            u = num_rational_in_r1
+            title = str(u)
+        data = get_rational_data_discrete(data_first_and_fin, l, u, c)
         round2_rationalities = [(1.0-d.rationality(2))*100.0 for d in data]
         ra = ResAcc(round2_rationalities)
         ra.compute_stats(True)
-        extra = '%u & %u & ' % (num_rational_in_r1, len(data))
+        extra = '%s & %u & ' % (title, len(data))
         print >> out, extra + ra.get_latex_row(precision, INTERVALS)
     print >> out, ResAcc.get_latex_table_footer()
 
 def main():
-    f = open('dat/simple_stats.tex', 'w')
-    simple_stats_table(f, 2)
-    f.close()
+    for i in [1, 3, 5, 10, 14]:
+        f = open('dat/simple_stats_%u.tex' % i, 'w')
+        simple_stats_table(f, 1, i)
+        f.close()
     return 0
 
 if __name__ == "__main__":
